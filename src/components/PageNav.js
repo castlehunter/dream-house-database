aef261c7d11a0ef8c28fed98cdcd6884bd07d855
castlehunter <vvxiaobai@gmail.com>
2024-08-02 16:53:16 -0400

    login button

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -1,17 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./PageNav.module.css";
 // import Logo from "../components/Logo";
 import logo from "../assets/logo.png";
 import Button from "./Button";
 
 function PageNav({ bgColor = "" }) {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/dashboard/");
+  }
+
   return (
     <nav className={styles.navigationBar} style={{ backgroundColor: bgColor }}>
       <div className={styles.logo}>
         <img src={logo} alt="logo" />
         <span className={styles.logoText}>Dream Home</span>
       </div>
-      <h4>Dream Home System</h4>
+      <Button onClick={handleClick}>Login</Button>
     </nav>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,16 +6,10 @@ import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-  const navigate = useNavigate();
-
   return (
     <main className={styles.homepage}>
       <PageNav bgColor="#000" />
-      <section>
-        <Button onClick={() => navigate("/dashboard")} classType="login">
-          Login
-        </Button>
-      </section>
+      <section></section>
       <Footer />
     </main>
   );
